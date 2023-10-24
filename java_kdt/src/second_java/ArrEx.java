4712a6fc6b82a69d03019f1bd0d2872457271e9b
Asong Ma <136426795+asonghub@users.noreply.github.com>
2023-10-24 16:40:56 +0900

    1023 ArrayList

--- java_kdt/src/second_java/ArrEx.java
+++ java_kdt/src/second_java/ArrEx.java
@@ -8,6 +8,8 @@ public class ArrEx {
 		
 //		int[] intArray = {1,2,3,4,5};
 //		
+		//배열 출력 기초 
+		//방법1 for문 활용 
 //		for(int i = 0; i < intArray.length; i++) {
 //			System.out.print(intArray[i]+ " ");
 //		}
@@ -19,9 +21,21 @@ public class ArrEx {
 		for( int i = 0 ; i < intArray.length; i++) {
 			intArray[i] = scan.nextInt();
 		}
-		//Array.toString 배열의 요소를 문자열로 반환 
+		//방법2 Array.toString 배열의 요소를 문자열로 반환 
+		//string으로 변환하지 않으면 이상한문자로 출력됨. 
 		System.out.println(Arrays.toString(intArray));
 		System.out.println(intArray);
+		
+		//방법3 for-each 
+		int sum = 0;
+		for( int arr: intArray) {
+			System.out.print(arr + " ");
+			sum += arr;
+		}
+		System.out.println(sum);
+		
+		
+		
 		
 		scan.close();
 	}
